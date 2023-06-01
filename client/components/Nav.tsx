9a977dfecada723bc898b67320b330a5ab6cc5ec
Gemma Krebs <96151596+ge-krebs@users.noreply.github.com>
2023-06-01 17:14:16 +1200

    Update Nav.tsx

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -2,12 +2,24 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
+    <nav>
     <ul>
-      <li><a href="#">home</a></li>
-      <li><a href="#">menu</a></li>
-      <li><a href="#">order</a></li>
-      <li><a href="#">lockers</a></li>
+      <Link className="test" key="homeBtn" to="/">
+        <button>home</button>
+      </Link>
+      <Link className="test" key="menuBtn" to="/menu">
+        <button>menu</button>
+      </Link>
+
+      <Link className="test" key="orderBtn" to="/order">
+        <button>order</button>
+      </Link>
+
+      <Link className="test" key="lockerBtn" to="/lockers">
+        <button>lockers</button>
+      </Link>
     </ul>
+    </nav>
   )
 }
 
