5c2df7504d540d2c81012450ea11e27bd397f22d
Gemma Krebs <96151596+ge-krebs@users.noreply.github.com>
2023-06-01 17:15:05 +1200

    Update router.tsx

--- client/router.tsx
+++ client/router.tsx
@@ -7,12 +7,16 @@ import {
 import App from './components/App'
 import Home from './components/Home'
 import Menu from './components/Menu'
+import Order from './components/Order'
+import Locker from './components/Locker'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/home" element={<Home />}></Route>
+      <Route path="/" element={<Home />}></Route>
       <Route path="/menu" element={<Menu />}></Route>
+      <Route path="/order" element={<Order />}></Route>
+      <Route path="/lockers" element={<Locker />}></Route>
     </Route>
   )
 )
