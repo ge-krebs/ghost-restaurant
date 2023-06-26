dc47c3bd675cb6778b6ceed2faeb595c12e6ecee
Gerard Paapu <gerard@gerardpaapu.com>
2023-06-27 10:42:00 +1200

    fix: add allowJS to tsconfig

---
+++ dist/server/db/migrations/20230607235429_lockers.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('lockers', (table) => {
+        table.increments('id').primary;
+        table.boolean('filled');
+    });
+};
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('lockers');
+};
--- dist/server/server.js
+++ dist/server/server.js
@@ -10,8 +10,14 @@ const orders_1 = __importDefault(require("./routes/orders"));
 const locker_1 = __importDefault(require("./routes/locker"));
 const server = (0, express_1.default)();
 server.use(express_1.default.json());
-server.use(express_1.default.static((0, node_path_1.join)(__dirname, 'public')));
 server.use('/api/v1/menu', menu_1.default);
 server.use('/api/v1/orders', orders_1.default);
 server.use('/api/v1/locker', locker_1.default);
+if (process.env.NODE_ENV === 'production') {
+    server.use('/assets', express_1.default.static((0, node_path_1.join)(__dirname, '../assets')));
+    server.use('/', express_1.default.static((0, node_path_1.join)(__dirname, '../public')));
+    server.get('*', (req, res) => {
+        res.sendFile((0, node_path_1.join)(__dirname, '../index.html'));
+    });
+}
 exports.default = server;
