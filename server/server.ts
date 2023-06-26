8d6a2ed970c8921f3cf60228fd8e1683291ea0af
Gerard Paapu <gerard@gerardpaapu.com>
2023-06-27 10:37:39 +1200

    fix: set up for prod deployment

--- server/server.ts
+++ server/server.ts
@@ -6,11 +6,17 @@ import locker from './routes/locker'
 
 const server = express()
 server.use(express.json())
-server.use(express.static(join(__dirname, 'public')))
 
 server.use('/api/v1/menu', menu)
 server.use('/api/v1/orders', orders)
 server.use('/api/v1/locker', locker)
 
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static(join(__dirname, '../assets')))
+  server.use('/', express.static(join(__dirname, '../public')))
+  server.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '../index.html'))
+  })
+}
 
 export default server
